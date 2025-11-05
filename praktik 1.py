abd4df03746e9e527c38dfbd14251c0aa586e904
faqih <faqi22152ti@student.nurulfikri.ac.id>
2025-11-05 09:38:41 +0700

    edit

--- praktik 1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("PRAKTIKUM 1 VISUALISASI DATA")
-st.subheader("Bagian 1. : Text Element")
-st.markdown("""
--Nama Lengkap Anggota
-1. Faizal Fandi Mulyadi
-2. Faqih Al Fauzan
-3. Affan cewe""")

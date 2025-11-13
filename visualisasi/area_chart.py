6d02e32650bd8a2999f17d79bf477fdf5156fde7
faizalfandimulyadi <faiz22084ti@student.nurulfikri.ac.id>
2025-11-13 21:52:09 +0700

    Initial commit

---
+++ visualisasi/area_chart.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Area Chart")
+st.subheader("Praktikum-2 Visualisasi Data")
+st.write("kelompok-3")
+st.markdown("""
+Nama Lengkap Anggota:
+1. Faizal Fandi Mulyadi - 011022084
+2. Faqih Al Fauzan - 0110222152
+3. AFNAN AINUL MARDHIYYAH - 0110222128
+""")
+
+df = pd.DataFrame(
+    np.random.randn(40, 4),
+    columns=["C1", "C2", "C3", "C4"]
+)
+
+st.area_chart(df)
